00efa7f6703fc71b109a84a8ef4f1e242788633d
Trung Kien <95215777+kiendoo4@users.noreply.github.com>
2022-02-21 14:10:58 +0700

    Create testing.cpp
    Build the initial home page

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"HELLO EVERYONE, I'M KIEN";
+    cout<<endl<<"I am a first-year student at the University of Information";
+    cout<<endl<<"I am 18 years old";
+    cout<<endl<<"My major is Computer Science :> ";
+    cout<<endl<<"Nice to meet you";
+    return 0;
+}
